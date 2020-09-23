4436e76d22e3fdad7c7359ad051f4f7f8117dd67
Mkushner17 <61876316+Mkushner17@users.noreply.github.com>
2020-09-23 07:21:11 -0400

    Add files via upload

---
+++ Database.py
@@ -0,0 +1,27 @@
+#Michael Kushner
+#CS-499
+#September 20, 2020
+#Ranjan, H. (2020, April 19). Python Program to find sum of array. Retrieved September 20, 2020,
+#   from https://www.geeksforgeeks.org/python-program-to-find-sum-of-array/
+# Python 3 code to find sum
+# of elements in given array
+def _sum(arr,n):
+
+    # return sum using sum
+    # inbuilt sum() function
+    return(sum(arr))
+
+# driver function
+arr=[]
+# input values to list
+arr = [3, 79, 85, 1]
+
+# calculating length of array
+n = len(arr)
+
+ans = _sum(arr,n)
+
+# display sum
+print ('Sum of the array is ', ans)
+
+# This code is contributed by Himanshu Ranjan
